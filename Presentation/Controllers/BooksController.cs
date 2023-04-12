e78b331f1f33bbcb46eea1ced7932a912157258b
Tuna GÜL <63519095+tunywalker@users.noreply.github.com>
2023-04-12 04:57:10 +0300

    NotFoundException and refactoring applied

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
@@ -23,86 +24,62 @@ namespace Presentation.Controllers
         [HttpGet]
         public IActionResult GetAllBooks()
         {
-            try
-            {
+          
                 var books = _manager.BookService.GetAllBooks(false);
                 return Ok(books);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            
+               
+            
         }
 
         [HttpGet("{id:int}")]
         public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
         {
-            try
-            {
+         
+               
                 var book = _manager
                 .BookService
                 .GetOneBookById(id, false);
 
-                if (book is null)
-                    return NotFound(); //404
-
                 return Ok(book);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+           
 
         }
 
         [HttpPost]
         public IActionResult CreateOneBook([FromBody] Book book)
         {
-            try
-            {
+            
                 if (book is null)
                     return BadRequest(); // 400 
 
                 _manager.BookService.CreateOneBook(book);
 
                 return StatusCode(201, book);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+          
         }
 
         [HttpPut("{id:int}")]
         public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id,
             [FromBody] Book book)
         {
-            try
-            {
+          
                 if (book is null)
                     return BadRequest(); // 400 
 
                 _manager.BookService.UpdateOneBook(id, book, true);
                 return NoContent(); // 204
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+           
         }
 
         [HttpDelete("{id:int}")]
         public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
         {
-            try
-            {
+         
                 _manager.BookService.DeleteOneBook(id, false);
                 return NoContent();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            
+           
         }
 
 
@@ -110,25 +87,19 @@ namespace Presentation.Controllers
         public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id,
             [FromBody] JsonPatchDocument<Book> bookPatch)
         {
-            try
-            {
+           
                 // check entity
                 var entity = _manager
                     .BookService
                     .GetOneBookById(id, true);
 
-                if (entity is null)
-                    return NotFound(); // 404
+               
 
                 bookPatch.ApplyTo(entity);
                 _manager.BookService.UpdateOneBook(id, entity, true);
 
                 return NoContent(); // 204
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+           
         }
     }
 }
