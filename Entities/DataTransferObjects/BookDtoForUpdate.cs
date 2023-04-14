d3e800a6ebb9bb99b535bbd8a236e3c7d4ff3353
Tuna GÜL <63519095+tunywalker@users.noreply.github.com>
2023-04-14 04:11:07 +0300

    -package install -define DTO -using _AutoMapper

---
+++ Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public record BookDtoForUpdate
+    {
+        public BookDtoForUpdate(int ıd, string title, decimal price)
+        {
+            Id = ıd;
+            Title = title;
+            Price = price;
+        } 
+
+        public int Id { get; init; }
+        public String Title { get; init; }
+        public decimal Price { get; init; } 
+    }
+}
